53d1c436413f502eb851d3c3fbc4fa151f69ea67
Maiguier <maxime.aiguier@epitech.eu>
2018-02-23 10:19:49 +0100

    (ADD) error handling

--- src/verif_map.c
+++ src/verif_map.c
@@ -27,9 +27,8 @@ int verif_number_and_point(sys_t *sys)
 	int a = 0;
 
 	if (sys->file[0] != '2' || sys->file[8] != '3'||
-	sys->file[14] != '4' || sys->file[24] != '5')
+	sys->file[16] != '4' || sys->file[24] != '5')
 		return (84);
-	// C'EST ICI QUE LE PROGRAMME RETURN 
 	for (int i = 1; i < 30;) {
 		if (sys->file[i] != ':')
 			return (84);
@@ -46,8 +45,8 @@ int verif_number_and_point(sys_t *sys)
 
 int verif_pos(sys_t *sys)
 {
-	int a = verif_number_and_point(sys);
-	// ICI LE A RETURN 84 ??
+	if (verif_number_and_point(sys) == 84)
+		return (84);
 	int n1[4] = {convert_letter(sys->file[2]),
 		convert_letter(sys->file[5]),
 		sys->file[3] - 47, sys->file[6] - 48};
@@ -61,10 +60,9 @@ int verif_pos(sys_t *sys)
 		convert_letter(sys->file[29]),
 		sys->file[27] - 44, sys->file[30] - 48};
 
-	if ((n1[0] == n1[1] && n1[2] != n1[3]) ||
-	(n2[0] == n2[1] && n2[2] != n2[3]) || (n3[0] == n3[1] && n3[2] != n3[3])
-	|| (n4[0] == n4[1] && n4[2] != n4[3]) || a == 84)
+	if ((n1[0] == n1[1] && n1[2] != n1[3]) || (n2[0] == n2[1] &&
+	n2[2] != n2[3]) || (n3[0] == n3[1] && n3[2] != n3[3])
+	|| (n4[0] == n4[1] && n4[2] != n4[3]))
 		return (84);
-	a = verif_all_ship(n1, n2, n3, n4);
-	return (a);
+	return (verif_all_ship(n1, n2, n3, n4));
 }
